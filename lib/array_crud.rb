dab163632082ca42613f505b458608243b4da966
Gabriel Chazanov <gabe.chaz@gmail.com>
2020-10-04 19:16:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -33,7 +33,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  babes = ["wow", "Sam", "Anna", "Katie", "Jess"]
+  return babes[0]
 end
 
 def retrieve_last_element_from_array(array)
